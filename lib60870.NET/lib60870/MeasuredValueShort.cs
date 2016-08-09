4a7e1e7ce56477e5d222d46a8358a9b81dab0a14
Michael Zillgith <michael.zillgith@mz-automation.de>
2016-08-09 18:25:40 +0200

    - added missing files

---
+++ lib60870.NET/lib60870/MeasuredValueShort.cs
@@ -0,0 +1,149 @@
+/*
+ *  MeasuredValueShortFloat.cs
+ *
+ *  Copyright 2016 MZ Automation GmbH
+ *
+ *  This file is part of lib60870.NET
+ *
+ *  lib60870.NET is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  lib60870.NET is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with lib60870.NET.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  See COPYING file for the complete license text.
+ */
+
+using System;
+
+namespace lib60870
+{
+	public class MeasuredValueShort : InformationObject
+	{
+		private float value;
+
+		public float Value {
+			get {
+				return this.value;
+			}
+		}
+
+		private QualityDescriptor quality;
+
+		public QualityDescriptor Quality {
+			get {
+				return this.quality;
+			}
+		}
+
+		public MeasuredValueShort (int objectAddress, float value, QualityDescriptor quality)
+			: base(objectAddress)
+		{
+			this.value = value;
+			this.quality = quality;
+		}
+
+
+		public MeasuredValueShort (ConnectionParameters parameters, byte[] msg, int startIndex) :
+			base(parameters, msg, startIndex)
+		{
+			startIndex += parameters.SizeOfIOA; /* skip IOA */
+
+			/* parse float value */
+			value = System.BitConverter.ToSingle (msg, startIndex);
+			startIndex += 4;
+
+			/* parse QDS (quality) */
+			quality = new QualityDescriptor (msg [startIndex++]);
+		}
+
+		public override void Encode(Frame frame, ConnectionParameters parameters) {
+			base.Encode(frame, parameters);
+
+			byte[] floatEncoded = BitConverter.GetBytes (value);
+
+			if (BitConverter.IsLittleEndian == false)
+				Array.Reverse (floatEncoded);
+
+			frame.AppendBytes (floatEncoded);
+
+			frame.SetNextByte (quality.EncodedValue);
+		}
+	}
+
+	public class MeasuredValueShortWithCP24Time2a : MeasuredValueShort
+	{
+
+
+		private CP24Time2a timestamp;
+
+		public CP24Time2a Timestamp {
+			get {
+				return this.timestamp;
+			}
+		}
+
+		public MeasuredValueShortWithCP24Time2a (int objectAddress, float value, QualityDescriptor quality, CP24Time2a timestamp)
+			: base(objectAddress, value, quality)
+		{
+			this.timestamp = timestamp;
+		}
+
+		public MeasuredValueShortWithCP24Time2a (ConnectionParameters parameters, byte[] msg, int startIndex) :
+			base(parameters, msg, startIndex)
+		{
+			startIndex += parameters.SizeOfIOA + 5; /* skip IOA */
+
+			/* parse CP56Time2a (time stamp) */
+			timestamp = new CP24Time2a (msg, startIndex);
+		}
+
+		public override void Encode(Frame frame, ConnectionParameters parameters) {
+			base.Encode(frame, parameters);
+
+			frame.AppendBytes (timestamp.GetEncodedValue ());
+		}
+
+	}
+
+	public class MeasuredValueShortWithCP56Time2a : MeasuredValueShort
+	{
+
+		private CP56Time2a timestamp;
+
+		public CP56Time2a Timestamp {
+			get {
+				return this.timestamp;
+			}
+		}
+
+		public MeasuredValueShortWithCP56Time2a (int objectAddress, float value, QualityDescriptor quality, CP56Time2a timestamp)
+			: base(objectAddress, value, quality)
+		{
+			this.timestamp = timestamp;
+		}
+
+		public MeasuredValueShortWithCP56Time2a (ConnectionParameters parameters, byte[] msg, int startIndex) :
+			base(parameters, msg, startIndex)
+		{
+			startIndex += parameters.SizeOfIOA + 5; /* skip IOA */
+
+			/* parse CP56Time2a (time stamp) */
+			timestamp = new CP56Time2a (msg, startIndex);
+		}
+
+		public override void Encode(Frame frame, ConnectionParameters parameters) {
+			base.Encode(frame, parameters);
+
+			frame.AppendBytes (timestamp.GetEncodedValue ());
+		}
+	}
+}
+
---
+++ lib60870.NET/tests/Test.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using lib60870;
+
+namespace tests
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestBCR ()
+		{
+			BinaryCounterReading bcr = new BinaryCounterReading ();
+
+			bcr.Value = 1000;
+
+			Assert.AreEqual (1000, bcr.Value);
+
+			bcr.Value = -1000;
+
+			Assert.AreEqual (-1000, bcr.Value);
+
+			bcr.SequenceNumber = 31;
+
+			Assert.AreEqual (31, bcr.SequenceNumber);
+
+			bcr.SequenceNumber = 0;
+
+			Assert.AreEqual (0, bcr.SequenceNumber);
+
+			/* Out of range sequenceNumber */
+			bcr.SequenceNumber = 32;
+
+			Assert.AreEqual (0, bcr.SequenceNumber);
+
+			bcr = new BinaryCounterReading ();
+
+			bcr.Invalid = true;
+
+			Assert.AreEqual (true, bcr.Invalid);
+			Assert.AreEqual (false, bcr.Carry);
+			Assert.AreEqual (false, bcr.Adjusted);
+			Assert.AreEqual (0, bcr.SequenceNumber);
+			Assert.AreEqual (0, bcr.Value);
+
+			bcr = new BinaryCounterReading ();
+
+			bcr.Carry = true;
+
+			Assert.AreEqual (false, bcr.Invalid);
+			Assert.AreEqual (true, bcr.Carry);
+			Assert.AreEqual (false, bcr.Adjusted);
+			Assert.AreEqual (0, bcr.SequenceNumber);
+			Assert.AreEqual (0, bcr.Value);
+
+			bcr = new BinaryCounterReading ();
+
+			bcr.Adjusted = true;
+
+			Assert.AreEqual (false, bcr.Invalid);
+			Assert.AreEqual (false, bcr.Carry);
+			Assert.AreEqual (true, bcr.Adjusted);
+			Assert.AreEqual (0, bcr.SequenceNumber);
+			Assert.AreEqual (0, bcr.Value);
+
+
+		}
+	}
+}
+
