89550211c13ecc046982b62e651b3f25bbff5297
Michael Zillgith <michael.zillgith@mz-automation.de>
2016-12-02 14:37:26 +0100

    - lib60870-C: changed version to 0.9.2

--- lib60870-C/src/inc/api/iec60870_common.h
+++ lib60870-C/src/inc/api/iec60870_common.h
@@ -33,7 +33,7 @@ extern "C" {
 
 #define LIB60870_VERSION_MAJOR 0
 #define LIB60870_VERSION_MINOR 9
-#define LIB60870_VERSION_PATCH 0
+#define LIB60870_VERSION_PATCH 2
 
 typedef struct {
     int major;
