6eadb693e2ecf69d9e54bf8e75f20f54f9d6e6f4
Michael Zillgith <michael.zillgith@mz-automation.de>
2017-02-25 11:52:46 +0100

    - lib60870.NET: added constructors and set properties for CP24Time2a

--- lib60870.NET/lib60870/CP24Time2a.cs
+++ lib60870.NET/lib60870/CP24Time2a.cs
@@ -42,6 +42,12 @@ namespace lib60870
 			for (int i = 0; i < 3; i++)
 				encodedValue [i] = 0;
 		}
+
+		public CP24Time2a(int minute, int second, int millisecond) {
+			Millisecond = millisecond;
+			Second = second;
+			Minute = minute;
+		}
 			
 		/// <summary>
 		/// Gets the total milliseconds of the elapsed time
@@ -55,27 +61,54 @@ namespace lib60870
 		}
 
 		/// <summary>
-		/// Gets the millisecond part of the time value
+		/// Gets or sets the millisecond part of the time value
 		/// </summary>
 		/// <value>The millisecond.</value>
 		public int Millisecond {
 			get {
 				return (encodedValue[0] + (encodedValue[1] * 0x100)) % 1000;
 			}
+
+			set {
+				int millies = (Second * 1000) + value;
+
+				encodedValue [0] = (byte)(millies & 0xff);
+				encodedValue [1] = (byte)((millies / 0x100) & 0xff);
+			}
 		}
 
+		/// <summary>
+		/// Gets or sets the second (range 0 to 59)
+		/// </summary>
+		/// <value>The second.</value>
 		public int Second {
 			get {
 				return  (encodedValue[0] + (encodedValue[1] * 0x100)) / 1000;
 			}
+
+			set {
+				int millies = encodedValue [0] + (encodedValue [1] * 0x100);
+
+				int msPart = millies % 1000;
+
+				millies = (value * 1000) + msPart;
+
+				encodedValue [0] = (byte)(millies & 0xff);
+				encodedValue [1] = (byte)((millies / 0x100) & 0xff);
+			}
 		}
 
+		/// <summary>
+		/// Gets or sets the minute (range 0 to 59)
+		/// </summary>
+		/// <value>The minute.</value>
 		public int Minute {
 			get {
 				return (encodedValue [2] & 0x3f);
 			}
+
 			set {
-				encodedValue [2] = (byte) (((byte) encodedValue [2] & (byte) 0xc0) + (byte)(value & 0x3f));
+				encodedValue [2] = (byte) ((encodedValue [2] & 0xc0) | (value & 0x3f));
 			}
 		}
 
