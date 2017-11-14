c5e8e8b1ee27d9cf8a218d30cc3312df7f68d609
Pelle van der Heide <P.S.vanderHeide@gmail.com>
2017-11-14 17:12:48 +0100

    Add wrapper for T104Slave
    Add simple_server.py, duplicating the functionality of the c program

---
+++ lib60870-Python/examples/simple_server/simple_server.py
@@ -0,0 +1,102 @@
+import logging
+import time
+
+from lib60870 import lib60870
+from lib60870.lib60870 import IEC60870ConnectionEvent, CauseOfTransmission, QualityDescriptor, TypeID
+from lib60870.T104Slave import T104Slave
+from lib60870.information_object import *
+from lib60870.asdu import ASDU
+
+logging.basicConfig(format='%(asctime)s %(name)s:%(levelname)s:%(message)s', level=logging.INFO)
+
+
+def clock_sync_callback(parameter, connection, asdu, newtime):
+    logging.info("Process time sync command with time {}".format(newtime))
+    return True
+
+
+def connection_request_callback(parameter, conn):
+    logging.info("New connection from {}".format(conn))
+    return True
+
+
+def interrogation_callback(parameter, connection, asdu, qoi):
+    logging.info("Received interrogation for group {}".format(qoi))
+    if qoi == 20:
+        connection.send_act_con(asdu)
+
+        new_asdu = ASDU(parameter, TypeID.M_ME_NB_1, False, CauseOfTransmission.INTERROGATED_BY_STATION, 0, 1)
+        new_asdu.add_information_object(MeasuredValueScaled(100, -1, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        new_asdu.add_information_object(MeasuredValueScaled(101, 23, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        new_asdu.add_information_object(MeasuredValueScaled(102, 2300, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        connection.send_asdu(new_asdu)
+
+        new_asdu = ASDU(parameter, TypeID.M_SP_NA_1, False, CauseOfTransmission.INTERROGATED_BY_STATION, 0, 1)
+        new_asdu.add_information_object(SinglePointInformation(104, True, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        new_asdu.add_information_object(SinglePointInformation(105, False, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        connection.send_asdu(new_asdu)
+
+        new_asdu = ASDU(parameter, TypeID.M_SP_NA_1, False, CauseOfTransmission.INTERROGATED_BY_STATION,  0, 1)
+        new_asdu.add_information_object(SinglePointInformation(301, False, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        new_asdu.add_information_object(SinglePointInformation(302, True, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        new_asdu.add_information_object(SinglePointInformation(303, False, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        new_asdu.add_information_object(SinglePointInformation(304, True, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        new_asdu.add_information_object(SinglePointInformation(305, False, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        new_asdu.add_information_object(SinglePointInformation(306, True, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        new_asdu.add_information_object(SinglePointInformation(307, False, QualityDescriptor.IEC60870_QUALITY_GOOD))
+        connection.send_asdu(new_asdu)
+
+        connection.send_act_term(asdu)
+    else:
+        connection.send_act_con(asdu, True)
+    return True
+
+
+def asdu_callback(parameter, connection, asdu):
+    logging.info("Received asdu {}".format(asdu))
+    if asdu.get_type_id() == TypeID.C_SC_NA_1:
+        logging.info("received single command\n")
+
+        if asdu.get_cot == CauseOfTransmission.ACTIVATION:
+            sc = asdu.elements[0]
+            if sc.get_object_address() == 5000:
+                logging.info("IOA: {} switch to {}".format(sc.get_object_address(), sc.get_state()))
+                cot = CauseOfTransmission.ACTIVATION_CON
+            else:
+                cot = CauseOfTransmission.UNKNOWN_INFORMATION_OBJECT_ADDRESS
+        else:
+            cot = CauseOfTransmission.UNKNOWN_CAUSE_OF_TRANSMISSION
+        asdu.set_cot(cot)
+        connection.send_asdu(asdu)
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    t104slave = T104Slave()
+    t104slave.set_local_address(ip=b"localhost")
+    connectionParameters = t104slave.get_connection_parameters()
+
+    # Set up callbacks
+    t104slave.set_connection_request_handler(connection_request_callback)
+    t104slave.set_clock_synchronization_handler(clock_sync_callback)
+    t104slave.set_interrogation_handler(interrogation_callback)
+    t104slave.set_asdu_handler(asdu_callback)
+
+    t104slave.start()
+    if not t104slave.is_running():
+        raise RuntimeError("Server not started")
+
+    try:
+        scaledValue = 0
+        while True:
+            time.sleep(1)
+            asdu = ASDU(connectionParameters, TypeID.M_ME_NB_1, False, CauseOfTransmission.PERIODIC, 0, 1, False, False)
+            asdu.add_information_object(MeasuredValueScaled(110, scaledValue, QualityDescriptor.IEC60870_QUALITY_GOOD))
+            scaledValue += 1
+            t104slave.enqueue_asdu(asdu)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        t104slave.stop()
---
+++ lib60870-Python/lib60870/T104Slave.py
@@ -0,0 +1,198 @@
+import logging
+import ctypes
+import time
+import enum
+from ctypes import *
+
+from lib60870.common import *
+from lib60870.CP56Time2a import CP56Time2a, pCP56Time2a
+from lib60870.asdu import pASDU
+from lib60870.information_object import pInformationObject
+from lib60870 import lib60870
+
+logger = logging.getLogger(__name__)
+lib = lib60870.get_library()
+
+# type aliases
+pT104Slave = ctypes.c_void_p
+pMasterConnection = ctypes.c_void_p
+c_enum = c_int
+
+
+class ServerMode(enum.Enum):
+    SINGLE_REDUNDANCY_GROUP = 0
+    CONNECTION_IS_REDUNDANCY_GROUP = 1
+
+    @property
+    def c_enum(self):
+        return ctypes.c_int(self.value)
+
+
+class MasterConnection():
+    def __init__(self, pointer):
+        self._pointer = pointer
+
+    def send_asdu(self, asdu):
+        lib.MasterConnection_sendASDU.restype = c_bool
+        return lib.MasterConnection_sendASDU(self.pointer, asdu.pointer)
+
+    def send_act_con(self, asdu, negative=False):
+        lib.MasterConnection_sendACT_CON.restype = c_bool
+        return lib.MasterConnection_sendACT_CON(self.pointer, asdu.pointer, c_bool(negative))
+
+    def send_act_term(self, asdu):
+        lib.MasterConnection_sendACT_TERM.restype = c_bool
+        return(lib.MasterConnection_sendACT_TERM(self.pointer, asdu.pointer))
+
+    def close(self):
+        lib.MasterConnection_close(self.pointer)
+
+    def deactivate(self):
+        lib.MasterConnection_deactivate(self.pointer)
+
+    @property
+    def pointer(self):
+        return pMasterConnection(self._pointer)
+
+
+class T104Slave():
+    def __init__(self, parameters=None, max_low_prio_queue_size=128, max_high_prio_queue_size=128):
+        logger.debug("calling T104Slave_create()")
+        lib.T104Slave_create.restype = pT104Slave
+        self.con = pT104Slave(
+            lib.T104Slave_create(
+                parameters,
+                c_int(max_low_prio_queue_size),
+                c_int(max_high_prio_queue_size)
+            )
+        )
+
+    def __del__(self):
+        self.destroy()
+
+    def set_local_address(self, ip):
+        logger.debug("calling T104Slave_setLocalAddress()")
+        lib.T104Slave_setLocalAddress(self.con, c_char_p(ip))
+
+    def set_local_port(self, port):
+        logger.debug("calling T104Slave_setLocalPort()")
+        lib.T104Slave_setLocalPort(self.con, c_int(port))
+
+    def get_connection_parameters(self):
+        logger.debug("calling Slave_getConnectionParameters()")
+        lib.Slave_getConnectionParameters.restype = pConnectionParameters
+        return lib.Slave_getConnectionParameters(self.con).contents
+
+    def get_open_connections(self):
+        logger.debug("calling T104Slave_getOpenConnections()")
+        lib.T104Slave_getOpenConnections.restype = c_int
+        return lib.T104Slave_getOpenConnections(self.con)
+
+    def set_max_open_connections(self, num_connections):
+        logger.debug("calling T104Slave_setMaxOpenConnections()")
+        lib.T104Slave_setMaxOpenConnections(self.con, c_int(num_connections))
+
+    def set_server_mode(self, mode):
+        logger.debug("calling T104Slave_setServerMode()")
+        lib.T104Slave_setServerMode(self.con, mode.c_value)
+
+    def start(self):
+        logger.debug("calling Slave_start()")
+        lib.Slave_start(self.con)
+
+    def stop(self):
+        logger.debug("calling Slave_stop()")
+        lib.Slave_stop(self.con)
+
+    def is_running(self):
+        logger.debug("calling Slave_isRunning()")
+        lib.Slave_isRunning.restype = c_bool
+        return lib.Slave_isRunning(self.con)
+
+    def enqueue_asdu(self, asdu):
+        logger.debug("calling Slave_enqueueASDU()")
+        lib.Slave_enqueueASDU(self.con, asdu.pointer)
+
+    def destroy(self):
+        logger.debug("calling Slave_destroy()")
+        lib.Slave_destroy(self.con)
+
+    def set_connection_request_handler(self, callback, parameter=None):
+        logger.debug("setting connection request handler")
+        ConnectionRequestHandler = ctypes.CFUNCTYPE(c_bool, c_void_p, c_char_p)
+
+        def wrapper(parameter, conn):
+            logger.debug("connection request event: {} {}".format(parameter, conn))
+            return callback(parameter, conn)
+
+        self._connection_request_handler = ConnectionRequestHandler(wrapper)
+        lib.T104Slave_setConnectionRequestHandler(self.con, self._connection_request_handler, parameter)
+
+    def set_interrogation_handler(self, callback, parameter=None):
+        logger.debug("setting interrogation callback")
+        InterrogationHandler = ctypes.CFUNCTYPE(c_bool, c_void_p, pMasterConnection, pASDU, c_uint8)
+
+        def wrapper(parameter, connection, asdu, qoi):
+            logger.debug("interrogation event: {} {} {} {}".format(parameter, connection, asdu, qoi))
+            connection = MasterConnection(connection)
+            return callback(parameter, connection, asdu.contents, qoi)
+
+        self._interrogation_handler = InterrogationHandler(wrapper)
+        lib.Slave_setInterrogationHandler(self.con, self._interrogation_handler, parameter)
+
+    def set_counter_interrogation_handler(self, callback, parameter=None):
+        logger.debug("setting counter interrogation callback")
+        CounterInterrogationHandler = ctypes.CFUNCTYPE(c_bool, c_void_p, pMasterConnection, pASDU, c_uint8)
+
+        def wrapper(parameter, connection, asdu, qcc):
+            logger.debug("counter interrogation event: {} {} {} {}".format(parameter, connection, asdu, qcc))
+            connection = MasterConnection(connection)
+            return callback(parameter, connection, asdu.contents, qcc)
+
+        self._counter_interrogation_handler = CounterInterrogationHandler(wrapper)
+        lib.Slave_setCounterInterrogationHandler(self.con, self._counter_interrogation_handler, parameter)
+
+    def set_read_handler(self, callback, parameter=None):
+        """
+        Set Handler for read command (C_RD_NA_1 - 102)
+        """
+        logger.debug("setting read callback")
+        ReadHandler = ctypes.CFUNCTYPE(c_bool, c_void_p, pMasterConnection, pASDU, c_uint8)
+
+        def wrapper(parameter, connection, asdu, ioa):
+            logger.debug("read event: {} {} {} {}".format(parameter, connection, asdu, ioa))
+            connection = MasterConnection(connection)
+            return callback(parameter, connection.contents, asdu.contents, ioa)
+
+        self._read_handler = ReadHandler(wrapper)
+        lib.Slave_setReadHandler(self.con, self._read_handler, parameter)
+
+    def set_clock_synchronization_handler(self, callback, parameter=None):
+        """
+        Handler for clock synchronization command (C_CS_NA_1 - 103)
+        """
+        logger.debug("setting clock synchronization callback")
+        ClockSynchronizationHandler = ctypes.CFUNCTYPE(c_bool, c_void_p, pMasterConnection, pASDU, pCP56Time2a)
+
+        def wrapper(parameter, connection, asdu, newtime):
+            logger.debug("clock sync event: {} {} {} {}".format(parameter, connection, asdu, newtime))
+            connection = MasterConnection(connection)
+            return callback(parameter, connection, asdu.contents, newtime.contents)
+
+        self._clock_sync_handler = ClockSynchronizationHandler(wrapper)
+        lib.Slave_setClockSyncHandler(self.con, self._clock_sync_handler, parameter)
+
+    def set_asdu_handler(self, callback, parameter=None):
+        """
+        Handler for ASDUs that are not handled by other handlers (default handler)
+        """
+        logger.debug("setting asdu callback")
+        ASDUHandler = ctypes.CFUNCTYPE(c_bool, c_void_p, pMasterConnection, pASDU)
+
+        def wrapper(parameter, connection, asdu):
+            logger.debug("asdu event: {} {} {}".format(parameter, connection, asdu))
+            connection = MasterConnection(connection)
+            return callback(parameter, connection, asdu.contents)
+
+        self._asdu_handler = ASDUHandler(wrapper)
+        lib.Slave_setASDUHandler(self.con, self._asdu_handler, parameter)
