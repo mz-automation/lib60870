bb8e53298dbe9d7d9a88a1baadf5b30389c1fdec
Michael Zillgith <michael.zillgith@mz-automation.de>
2018-08-08 17:08:15 +0200

    - added test cases for EventOfProtectionEquipmentWithCP56Time2a

--- lib60870-C/tests/all_tests.c
+++ lib60870-C/tests/all_tests.c
@@ -1,10 +1,26 @@
 #include "unity.h"
 #include "iec60870_common.h"
 #include "hal_time.h"
+#include "buffer_frame.h"
 
 void setUp(void) { }
 void tearDown(void) {}
 
+static struct sCS101_AppLayerParameters defaultAppLayerParameters = {
+    /* .sizeOfTypeId =  */ 1,
+    /* .sizeOfVSQ = */ 1,
+    /* .sizeOfCOT = */ 2,
+    /* .originatorAddress = */ 0,
+    /* .sizeOfCA = */ 2,
+    /* .sizeOfIOA = */ 3,
+    /* .maxSizeOfASDU = */ 249
+};
+
+void
+CS101_ASDU_encode(CS101_ASDU self, Frame frame);
+
+CS101_ASDU
+CS101_ASDU_createFromBuffer(CS101_AppLayerParameters parameters, uint8_t* msg, int msgLength);
 
 void
 test_CP56Time2a(void)
@@ -107,6 +123,63 @@ test_addMaxNumberOfIOsToASDU(void)
 }
 
 
+void
+test_SingleEventType(void)
+{
+    tSingleEvent singleEvent = 0;
+
+    EventState eventState = SingleEvent_getEventState(&singleEvent);
+
+    TEST_ASSERT_EQUAL_INT(IEC60870_EVENTSTATE_INDETERMINATE_0, eventState);
+
+    QualityDescriptorP qdp = SingleEvent_getQDP(&singleEvent);
+
+    TEST_ASSERT_EQUAL_INT(0, qdp);
+}
+
+void
+test_EventOfProtectionEquipmentWithTime(void)
+{
+    tSingleEvent singleEvent = 0;
+    struct sCP16Time2a elapsedTime;
+    struct sCP56Time2a timestamp;
+
+    EventOfProtectionEquipmentWithCP56Time2a e = EventOfProtectionEquipmentWithCP56Time2a_create(NULL, 1, &singleEvent, &elapsedTime, &timestamp);
+
+    uint8_t buffer[256];
+
+    struct sBufferFrame bf;
+
+    Frame f = BufferFrame_initialize(&bf, buffer, 0);
+
+    CS101_ASDU asdu = CS101_ASDU_create(&defaultAppLayerParameters, false, CS101_COT_PERIODIC, 0, 1, false, false);
+
+    CS101_ASDU_addInformationObject(asdu, (InformationObject) e);
+    CS101_ASDU_addInformationObject(asdu, (InformationObject) e);
+
+    CS101_ASDU_encode(asdu, f);
+
+    InformationObject_destroy(e);
+    CS101_ASDU_destroy(asdu);
+
+    CS101_ASDU asdu2 = CS101_ASDU_createFromBuffer(&defaultAppLayerParameters, buffer, Frame_getMsgSize(f));
+
+    InformationObject io = CS101_ASDU_getElement(asdu2, 1);
+
+    TEST_ASSERT_NOT_NULL(io);
+
+    EventOfProtectionEquipmentWithCP56Time2a e2 = (EventOfProtectionEquipmentWithCP56Time2a) io;
+
+    SingleEvent se = EventOfProtectionEquipmentWithCP56Time2a_getEvent(e2);
+
+    QualityDescriptorP qdp = SingleEvent_getQDP(se);
+
+    InformationObject_destroy(io);
+    CS101_ASDU_destroy(asdu2);
+
+    TEST_ASSERT_EQUAL_INT(0, qdp);
+}
+
 int
 main(int argc, char** argv)
 {
@@ -115,5 +188,7 @@ main(int argc, char** argv)
     RUN_TEST(test_CP56Time2aToMsTimestamp);
     RUN_TEST(test_StepPositionInformation);
     RUN_TEST(test_addMaxNumberOfIOsToASDU);
+    RUN_TEST(test_SingleEventType);
+    RUN_TEST(test_EventOfProtectionEquipmentWithTime);
     return UNITY_END();
 }
