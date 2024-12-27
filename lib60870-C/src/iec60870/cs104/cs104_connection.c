0e2501fceaa33a523ec05cca3a74e8d90fb329a5
Michael Zillgith <michael.zillgith@mz-automation.de>
2024-12-27 17:47:01 +0000

    - CS104 connection: fixed double free problem with TLSSocket when reusing the connection object (see #159)

--- lib60870-C/src/iec60870/cs104/cs104_connection.c
+++ lib60870-C/src/iec60870/cs104/cs104_connection.c
@@ -1031,7 +1031,10 @@ handleConnection(void* parameter)
 
     #if (CONFIG_CS104_SUPPORT_TLS == 1)
         if (self->tlsSocket)
+        {
             TLSSocket_close(self->tlsSocket);
+            self->tlsSocket = NULL;
+        }
     #endif
 
         Socket_destroy(self->socket);
